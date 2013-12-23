47a8f2083867595ff04df80b0278054aa97f609e
S Karthikeyan <karthikdevel@gmail.com>
2013-12-23 18:51:31 +0530

    Cleanup
    Making sure we do not loose reerences to some widgets.

--- gui/fitmain.py
+++ gui/fitmain.py
@@ -19,83 +19,83 @@ class FitTopFrame(wx.Frame):
         self.process_list = ['Novas','vim']
         self.params_list = ['mem','CPU']
 
-        panel = wx.Panel(self, -1)
+        self.panel = wx.Panel(self, -1)
         
         # top level sizer.
-        main_grid_sizer = wx.GridSizer(2,3,5,10)
+        self.main_grid_sizer = wx.GridSizer(2,3,5,10)
 
         # Static box for each list box
-        user_static_box= wx.StaticBox(panel, wx.ID_ANY, "User List")
-        user_listbox_sizer = wx.StaticBoxSizer(user_static_box,wx.VERTICAL)
-        process_static_box= wx.StaticBox(panel, wx.ID_ANY, label="Process List")
-        process_listbox_sizer = wx.StaticBoxSizer(process_static_box,wx.VERTICAL)
-        param_static_box= wx.StaticBox(panel, wx.ID_ANY, label="Param List")
-        param_listbox_sizer = wx.StaticBoxSizer(param_static_box,wx.VERTICAL)
+        user_static_box = wx.StaticBox(self.panel, wx.ID_ANY, "User List")
+        self.user_static_box = wx.StaticBoxSizer(user_static_box,wx.VERTICAL)
+        process_static_box = wx.StaticBox(self.panel, wx.ID_ANY, label="Process List")
+        self.process_listbox_sizer = wx.StaticBoxSizer(process_static_box,wx.VERTICAL)
+        param_static_box = wx.StaticBox(self.panel, wx.ID_ANY, label="Param List")
+        self.param_listbox_sizer = wx.StaticBoxSizer(param_static_box,wx.VERTICAL)
 
         # User List Box
-        self.user_listbox = wx.ListBox(panel, wx.ID_ANY, wx.DefaultPosition, (170, 130), self.user_list, wx.LB_MULTIPLE)
+        self.user_listbox = wx.ListBox(self.panel, wx.ID_ANY, wx.DefaultPosition, (170, 130), self.user_list, wx.LB_MULTIPLE)
         self.user_listbox.SetSelection(0)
         # Check Box to select all users
-        self.all_user_cb = wx.CheckBox(panel, wx.ID_ANY, 'Select All')
+        self.all_user_cb = wx.CheckBox(self.panel, wx.ID_ANY, 'Select All')
         self.all_user_cb.SetValue(False)
 
         # Process List Box
-        self.process_listbox = wx.ListBox(panel, wx.ID_ANY, wx.DefaultPosition, (170, 130), self.process_list, wx.LB_MULTIPLE)
+        self.process_listbox = wx.ListBox(self.panel, wx.ID_ANY, wx.DefaultPosition, (170, 130), self.process_list, wx.LB_MULTIPLE)
         self.process_listbox.SetSelection(0)
         # Check Box to select all processes
-        self.all_process_cb = wx.CheckBox(panel, wx.ID_ANY, 'Select All')
+        self.all_process_cb = wx.CheckBox(self.panel, wx.ID_ANY, 'Select All')
         self.all_process_cb.SetValue(False)
 
         # Param List Box
-        self.param_listbox = wx.ListBox(panel, wx.ID_ANY, wx.DefaultPosition, (170, 130), self.params_list, wx.LB_SINGLE)
+        self.param_listbox = wx.ListBox(self.panel, wx.ID_ANY, wx.DefaultPosition, (170, 130), self.params_list, wx.LB_SINGLE)
         self.param_listbox.SetSelection(0)
 
-        user_listbox_sizer.Add(self.user_listbox, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
-        user_listbox_sizer.Add(self.all_user_cb, 0, wx.BOTTOM | wx.CENTER)
-        process_listbox_sizer.Add(self.process_listbox, proportion = 1,  flag = wx.ALL | wx.EXPAND, border = 10)
-        process_listbox_sizer.Add(self.all_process_cb, 0, wx.BOTTOM | wx.CENTER)
-        param_listbox_sizer.Add(self.param_listbox, proportion = 1,  flag = wx.ALL | wx.EXPAND, border = 10)
+        self.user_static_box.Add(self.user_listbox, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
+        self.user_static_box.Add(self.all_user_cb, 0, wx.BOTTOM | wx.CENTER)
+        self.process_listbox_sizer.Add(self.process_listbox, proportion = 1,  flag = wx.ALL | wx.EXPAND, border = 10)
+        self.process_listbox_sizer.Add(self.all_process_cb, 0, wx.BOTTOM | wx.CENTER)
+        self.param_listbox_sizer.Add(self.param_listbox, proportion = 1,  flag = wx.ALL | wx.EXPAND, border = 10)
 
         # Add Each list box to main grid sizer.
-        main_grid_sizer.Add(user_listbox_sizer, proportion = 1,  flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
+        self.main_grid_sizer.Add(self.user_static_box, proportion = 1,  flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
 
-        main_grid_sizer.Add(process_listbox_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
+        self.main_grid_sizer.Add(self.process_listbox_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
 
-        main_grid_sizer.Add(param_listbox_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
+        self.main_grid_sizer.Add(self.param_listbox_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
         
         # Static box for each type of plot (Summary,Time plots & Scatter plots)
-        summary_plot_static_box= wx.StaticBox(panel, wx.ID_ANY, "Summary")
-        summary_plot_buttons_sizer = wx.StaticBoxSizer(summary_plot_static_box,wx.VERTICAL)
+        summary_plot_static_box = wx.StaticBox(self.panel, wx.ID_ANY, "Summary")
+        self.summary_plot_static_box_sizer  = wx.StaticBoxSizer(summary_plot_static_box, wx.VERTICAL)
 
-        time_plot_static_box= wx.StaticBox(panel, wx.ID_ANY, "Time")
-        time_plot_buttons_sizer = wx.StaticBoxSizer(time_plot_static_box,wx.VERTICAL)
+        time_plot_static_box = wx.StaticBox(self.panel, wx.ID_ANY, "Time")
+        self.time_plot_static_box_sizer = wx.StaticBoxSizer(time_plot_static_box,wx.VERTICAL)
 
-        scatter_plot_static_box= wx.StaticBox(panel, wx.ID_ANY, "Scatter")
-        scatter_plot_buttons_sizer = wx.StaticBoxSizer(scatter_plot_static_box,wx.VERTICAL)
+        scatter_plot_static_box = wx.StaticBox(self.panel, wx.ID_ANY, "Scatter")
+        self.scatter_plot_static_box_sizer = wx.StaticBoxSizer(scatter_plot_static_box,wx.VERTICAL)
         
         # Buttons for each type of plot.
-        self.user_vs_param_summary_btn = wx.Button(panel, wx.ID_ANY, 'User(s) vs Param')
-        self.process_vs_param_summary_btn = wx.Button(panel, wx.ID_ANY, 'Process(s) vs Param')
+        self.user_vs_param_summary_btn = wx.Button(self.panel, wx.ID_ANY, 'User(s) vs Param')
+        self.process_vs_param_summary_btn = wx.Button(self.panel, wx.ID_ANY, 'Process(s) vs Param')
 
-        summary_plot_buttons_sizer.Add(self.user_vs_param_summary_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
-        summary_plot_buttons_sizer.Add(self.process_vs_param_summary_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)        
+        self.summary_plot_static_box_sizer.Add(self.user_vs_param_summary_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
+        self.summary_plot_static_box_sizer.Add(self.process_vs_param_summary_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)        
 
-        self.peruserparam_vs_time_btn = wx.Button(panel, wx.ID_ANY, 'Time vs per User Param')
-        self.perprocessparam_vs_time_btn = wx.Button(panel, wx.ID_ANY, 'Time vs per Process Param')
+        self.peruserparam_vs_time_btn = wx.Button(self.panel, wx.ID_ANY, 'Time vs per User Param')
+        self.perprocessparam_vs_time_btn = wx.Button(self.panel, wx.ID_ANY, 'Time vs per Process Param')
         
-        time_plot_buttons_sizer.Add(self.peruserparam_vs_time_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
-        time_plot_buttons_sizer.Add(self.perprocessparam_vs_time_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
+        self.time_plot_static_box_sizer.Add(self.peruserparam_vs_time_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
+        self.time_plot_static_box_sizer.Add(self.perprocessparam_vs_time_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
 
-        self.user_vs_process_btn = wx.Button(panel, wx.ID_ANY, 'User vs Process')
+        self.user_vs_process_btn = wx.Button(self.panel, wx.ID_ANY, 'User vs Process')
         
-        scatter_plot_buttons_sizer.Add(self.user_vs_process_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
+        self.scatter_plot_static_box_sizer.Add(self.user_vs_process_btn, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
         
         # Add each plot category to the correct grid.
-        main_grid_sizer.Add(summary_plot_buttons_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
-        main_grid_sizer.Add(time_plot_buttons_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
-        main_grid_sizer.Add(scatter_plot_buttons_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
+        self.main_grid_sizer.Add(self.summary_plot_static_box_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
+        self.main_grid_sizer.Add(self.time_plot_static_box_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
+        self.main_grid_sizer.Add(self.scatter_plot_static_box_sizer, proportion = 1, flag = wx.ALL | wx.EXPAND | wx.ALIGN_TOP)
         
-        panel.SetSizerAndFit(main_grid_sizer)
+        self.panel.SetSizerAndFit(self.main_grid_sizer)
 
 class FitApp(wx.App):
     def OnInit(self):
